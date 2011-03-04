1aeb278afec6e0a8ab111a7073ae7ecb22168c63
Craig H Anderson <crai@coot.net>
2011-03-03 19:29:56 -0700

    Will push to git@heroku.com:cootcraig.git Clean rails 3.0.5 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cootcraig::Application.config.secret_token = 'ede48e81fae31895be8771f2190c62a8ab91d698c9be2c3b6715bde9640d472b0948f903a230e0203107524b5ec617a22af63d5255527cd659479c3e79623491'
